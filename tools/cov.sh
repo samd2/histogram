e7dcc7b0f742c37d6c8f235dffd8fe1d7c607d89
sdarwin <samuel.d.darwin@gmail.com>
2021-04-28 19:40:26 +0000

    clean up files

--- tools/cov.sh
+++ tools/cov.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-set -ex
-
 # must be executed in project root folder
 
 # Copyright Hans Dembinski 2018-2019
 # Distributed under the Boost Software License, Version 1.0.
 # See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
 
+set -ex
+
 if [ -z $GCOV ]; then
-  # gcov-10, gcov-9, gcov-7, gcov-6 do not work
   for i in 8 5; do
     if test $(which gcov-$i); then
       GCOV=gcov-$i
@@ -27,6 +26,7 @@ if [ ! -e $LCOV_DIR ]; then
   cd ..
 fi
 
+# For clang coverage
 if [ -n "$LLVM_COV_PATH" ]; then
   sudo ln -s $LLVM_COV_PATH /usr/bin/llvm-cov
 else
@@ -36,30 +36,20 @@ fi
 mkdir -p ~/.local/bin
 echo -e '#!/bin/bash\nexec llvm-cov gcov "$@"' > ~/.local/bin/gcov_for_clang.sh
 chmod 755 ~/.local/bin/gcov_for_clang.sh
-echo "Checking env"
-env
-
-echo "Checking for llvm-cov-11"
-which llvm-cov-11 || true
 
 # --rc lcov_branch_coverage=1 doesn't work on travis
 # LCOV="${LCOV_DIR}/bin/lcov --gcov-tool=${GCOV} --rc lcov_branch_coverage=1"
 LCOV="${LCOV_DIR}/bin/lcov --gcov-tool=${GCOV}"
 
-echo "starting first run of lcov"
-# collect raw data
+echo "collect raw data"
 $LCOV --base-directory `pwd`/../../ \
   --directory `pwd`/../../bin.v2/libs/histogram/test \
   --capture --output-file coverage.info
-
 echo "done with first run of lcov"
-ls -al coverage.info
 
-# remove uninteresting entries
+echo "remove uninteresting entries"
 $LCOV --extract coverage.info "*/boost/histogram/*" --output-file coverage.info
-
 echo "done with extraction"
-ls -al coverage.info
 
 if [ $1 ]; then
   echo "Uploading to cpp-coveralls"
